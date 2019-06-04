e4330a18d4e5374dc8a95ae4c852c01b1d315474
Hien Nguyen <nguyenphuchien1996@gmail.com>
2019-06-04 15:59:31 +0700

    init app

---
+++ src/config/styleGlobal.ts
@@ -0,0 +1,9 @@
+import { StyleSheet, Platform } from 'react-native';
+import { ifIphoneX } from 'react-native-iphone-x-helper';
+import { moderateScale } from 'react-native-size-matters';
+
+export const stylesGlobal = StyleSheet.create({
+
+});
+
+export default stylesGlobal;
