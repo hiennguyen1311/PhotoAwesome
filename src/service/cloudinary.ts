f947e5d3162e2754cf45f7c991b1f7c922b65134
Hien Nguyen <nguyenphuchien1996@gmail.com>
2019-06-20 17:16:23 +0700

    Update cloudinary

--- src/service/cloudinary.ts
+++ src/service/cloudinary.ts
@@ -1,16 +1,9 @@
-import RNCloudinary from 'react-native-cloudinary-x'
 const API_KEY = '843391347736845'
 const API_SECRET = 'XdRYnQZAjXd9UWZpvNmBjGxvN1E'
 const CLOUD_NAME = 'beyond-anttech';
 const folder = 'PhotoAwesome';
 
-RNCloudinary.init(API_KEY,API_SECRET, CLOUD_NAME);
 
 export const uploadImage = (uri: string) => {
-  RNCloudinary.UploadImage(uri).then((res: any) => {
-    console.log(res)
-  }) 
-  .catch((err: any) => {
-    console.warn(err)
-  })
+
 }
